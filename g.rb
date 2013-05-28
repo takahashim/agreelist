ab528c7486efd674a3a56e1db75cf919ed76e50d
Hector Perez <hecpeare@gmail.com>
2013-05-28 20:35:28 +0200

    minor change

--- g.rb
+++
@@ -1,8 +0,0 @@
-require 'net/http'
-
-Net::HTTP.start("http://api.twitter.com") { |http|
-  resp = http.get("/1/users/profile_image/alfranken.png?size=mini")
-  open("pie.png" ,"wb") { |file|
-    file.write(resp.body)
-  }
-}

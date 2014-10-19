aaaeed55cb1938e5e7c29cd3bc606e37fb08117a
Hector Perez <hecpeare@gmail.com>
2014-10-19 12:04:16 +0100

    rm fetching twitter image for now

--- app/models/individual.rb
+++ app/models/individual.rb
@@ -8,19 +8,19 @@ class Individual < ActiveRecord::Base
 
   validates :name, :presence => true
 
-  before_save :update_picture_from_twitter
-  
-  def update_picture_from_twitter
-    unless twitter.blank?
-      url = Twitter.user(twitter).attrs[:profile_image_url_https]
-      self.picture = open(url)
-    end
-  end
+  # before_save :update_picture_from_twitter
+
+  # def update_picture_from_twitter
+  #   unless twitter.blank?
+  #     url = Twitter.user(twitter).attrs[:profile_image_url_https]
+  #     self.picture = open(url)
+  #   end
+  # end
 
   def self.find_or_create(name)
     self.find_by_name(name) || self.create(name: name)
   end
-  
+
   def agrees
     agreements.select{ |a| a.extent == 100 }.map{ |i| i.statement }
   end
@@ -28,7 +28,7 @@ class Individual < ActiveRecord::Base
   def disagrees
     agreements.select{ |a| a.extent == 0 }.map{ |i| i.statement }
   end
-  
+
   def tag_list
     tags.map(&:name).join(", ")
   end
@@ -42,4 +42,5 @@ class Individual < ActiveRecord::Base
   def self.search(search)
     search.blank? ? [] : self.where("content LIKE ?", "%#{search}%")
   end
+
 end

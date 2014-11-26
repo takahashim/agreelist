6ef115eaabaeae5eb0ce4a3f01019ec84fecfb78
Hector Perez <hecpeare@gmail.com>
2014-11-26 21:56:44 +0000

    fix 2 rows from home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @statements = [1, 3, 4, 5, 6, 7].map{ |s| Statement.find(s) }
+    # @statements = [1, 2, 3, 1, 2, 3].map{ |s| Statement.find(s) }
   end
 
   def contact

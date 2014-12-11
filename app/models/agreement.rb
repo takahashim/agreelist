4209cfd440f56dcacd596c5136fb9322ef8765c1
Hector Perez <hecpeare@gmail.com>
2014-12-11 20:53:59 +0000

    short url for statements#show

--- app/models/agreement.rb
+++ app/models/agreement.rb
@@ -2,7 +2,11 @@ class Agreement < ActiveRecord::Base
   attr_accessible :individual_id, :statement_id, :url, :extent
   validates :individual_id, presence:true
   validates :statement_id, presence:true
-  
+
   belongs_to :statement
   belongs_to :individual
+
+  def short_url
+    url.gsub(/.*http:\/\//,'').gsub(/.*www\./,'')[0..15] + "..."
+  end
 end

d0de28cef39d6ee9d5b86729e47269e4d42333fe
Hector Perez <hecpeare@gmail.com>
2014-12-11 20:13:53 +0000

    same homepage in development than in production

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    if Rails.env == "production"
-      @statements = [1, 3, 4, 5, 6, 7].map{ |s| Statement.find(s) }
-    else
-      @statements = [1, 1, 1, 1, 1, 1].map{ |s| Statement.find(s) }
-    end
+    @statements = [1, 3, 4, 5, 6, 7].map{ |s| Statement.find(s) }
     # @statements = [1, 2, 3, 1, 2, 3].map{ |s| Statement.find(s) }
   end
 

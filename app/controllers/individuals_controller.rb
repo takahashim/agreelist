1bd28438dae5964822a054a5ee16d7006399adc7
Hector Perez <hecpeare@gmail.com>
2014-12-17 20:19:23 +0000

    fix individuals controller update

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -15,7 +15,7 @@ class IndividualsController < ApplicationController
     @individual = Individual.find(params[:id])
 
     respond_to do |format|
-      if @individual.update_attributes(params[:individual])
+      if @individual.update_attributes(params.require(:individual).permit(:name, :twitter))
         format.html { redirect_to @individual, notice: 'Statement was successfully updated.' }
         format.json { head :no_content }
       else

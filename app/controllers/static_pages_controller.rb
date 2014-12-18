4c6e801205055367867e22c226fd773be01e63ee
Hector Perez <hecpeare@gmail.com>
2014-12-18 19:59:34 +0000

    Blair to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -21,7 +21,7 @@ class StaticPagesController < ApplicationController
       end
     else
       @statements = [1, 3, 8, 5, 6, 7].map{ |s| Statement.find(s) }
-      @individuals = [5, 6, 1, 18, 17, 11].map{ |i| Individual.find(i) }
+      @individuals = [5, 6, 1, 18, 17, 12].map{ |i| Individual.find(i) }
     end
   end
 

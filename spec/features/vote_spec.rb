d7075978bf50116e40b0601a7b7cd3542406e44e
Hector Perez <hecpeare@gmail.com>
2014-12-18 19:50:50 +0000

    vote spec

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -5,6 +5,7 @@ feature 'disagree' do
     statement = Statement.create(content: "aaa")
     expect(statement.valid?).to eq true
     visit "/statements/#{statement.id}"
+    http_login
   end
 
   scenario 'adds someone who disagrees' do
@@ -14,5 +15,21 @@ feature 'disagree' do
 
     click_button "Add"
     expect(Agreement.last.disagree?).to eq(true)
+  end
+
+  def http_login
+    name = 'hector'
+    password = 'perez'
+    # request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
+
+    if page.driver.respond_to?(:basic_auth)
+      page.driver.basic_auth(name, password)
+    elsif page.driver.respond_to?(:basic_authorize)
+      page.driver.basic_authorize(name, password)
+    elsif page.driver.respond_to?(:browser) && page.driver.browser.respond_to?(:basic_authorize)
+      page.driver.browser.basic_authorize(name, password)
+    else
+      raise "I don't know how to log in!"
+    end
   end
 end

3aa76fc9368e9641755f20a0015e9058b815d615
Hector Perez <hecpeare@gmail.com>
2014-12-25 11:59:21 +0100

    change home page statement

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -20,8 +20,8 @@ class StaticPagesController < ApplicationController
         @individuals << Individual.first
       end
     else
-      @statements = [1, 3, 8, 5, 6, 7, 9, 13, 14].map{ |s| Statement.find(s) }
-      @individuals = [5, 6, 1, 18, 17, 12, 19, 23, 24].map{ |i| Individual.find(i) }
+      @statements = [1, 3, 8, 5, 6, 7, 15, 13, 14].map{ |s| Statement.find(s) }
+      @individuals = [5, 6, 1, 18, 17, 12, 25, 23, 24].map{ |i| Individual.find(i) }
     end
   end
 

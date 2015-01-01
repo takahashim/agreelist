d442c5fb1e287b623e03be618dec34ed7e57ca7e
Hector Perez <hecpeare@gmail.com>
2015-01-01 14:26:03 +0100

    new statement from home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -23,6 +23,7 @@ class StaticPagesController < ApplicationController
       @statements = [1, 3, 8, 5, 6, 7, 15, 13, 14].map{ |s| Statement.find(s) }
       @individuals = [5, 6, 1, 18, 17, 12, 25, 23, 24].map{ |i| Individual.find(i) }
     end
+    @statement = Statement.new if current_user
   end
 
   def contact

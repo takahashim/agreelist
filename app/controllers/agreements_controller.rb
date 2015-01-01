948c494699f16b7fe632e7c7d2032c94f973b8d1
Hector Perez <hecpeare@gmail.com>
2015-01-01 17:35:38 +0100

    signed_in? method

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -1,10 +1,11 @@
 class AgreementsController < ApplicationController
-  http_basic_authenticate_with name: "hector", password: "perez"
   def destroy
-    @agreement = Agreement.find(params[:id])
-    statement_id = @agreement.statement_id
-    @agreement.destroy
+    if admin?
+      @agreement = Agreement.find(params[:id])
+      statement_id = @agreement.statement_id
+      @agreement.destroy
 
-    redirect_to statement_path(statement_id)
+      redirect_to statement_path(statement_id)
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user
+  helper_method :current_user, :signed_in?
 
   private
 
   def current_user
     @current_user ||= Individual.find(session[:user_id]) if session[:user_id]
+  end
+
+  def signed_in?
+    current_user.present?
+  end
+
+  def admin?
+    current_user.try(:twitter) == "arpahector"
   end
 end

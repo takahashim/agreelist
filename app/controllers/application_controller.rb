3035be753dda63a959fdb7ac3f435c28b54e25b6
Hector Perez <hecpeare@gmail.com>
2015-01-05 09:07:22 +0000

    rm delete statement link for non admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user, :signed_in?
+  helper_method :current_user, :signed_in?, :admin?
 
   private
 

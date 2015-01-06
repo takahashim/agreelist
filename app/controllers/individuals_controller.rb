98b82573902b3a0eb31332684fa1cc091f6fc268
Hector Perez <hecpeare@gmail.com>
2015-01-06 19:19:18 +0000

    refactorize individuals controller

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -1,27 +1,31 @@
 class IndividualsController < ApplicationController
   before_action :login_required, only: :update
+  before_action :load_individual
+  before_action :same_user_required, only: :update
 
   def show
-    @individual = Individual.find(params[:id])
     @agrees = @individual.agrees
     @disagrees = @individual.disagrees
   end
 
   def edit
-    @individual = Individual.find(params[:id])
   end
 
   def update
-    @individual = Individual.find(params[:id])
-
-    if @individual == current_user || admin?
-      if @individual.update_attributes(params.require(:individual).permit(:name, :twitter, :email))
-        redirect_to root_path, notice: 'Successfully updated.'
-      else
-        render action: "edit"
-      end
+    if @individual.update_attributes(params.require(:individual).permit(:name, :twitter, :email))
+      redirect_to root_path, notice: 'Successfully updated.'
     else
-      redirect_to root_path, notice: "You can't edit other user's profile"
+      render action: "edit"
     end
+  end
+
+  private
+
+  def load_individual
+    @individual = Individual.find(params[:id])
+  end
+
+  def same_user_required
+    redirect_to root_path, notice: "You can't edit other user's profile"  if @individual != current_user && !admin?
   end
 end

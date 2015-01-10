a2053a237f2d4113ecddc9d0215c97738b15b71e
Hector Perez <hecpeare@gmail.com>
2015-01-10 20:05:06 +0000

    explicit from parameter in log mailer

--- app/mailers/log_mailer.rb
+++ app/mailers/log_mailer.rb
@@ -1,8 +1,6 @@
 class LogMailer < ActionMailer::Base
-  default from: "feedback@agreelist.com"
-
   def log_email(text)
     @text = text
-    mail(to: "hecpeare@gmail.com", subject: text)
+    mail(to: "hecpeare@gmail.com", from: "feedback@agreelist.com", subject: text)
   end
 end

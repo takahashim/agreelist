29fb4bc7af1ba3d6fec9e37d3dd1b27dd57d8b4f
Hector Perez <hecpeare@gmail.com>
2015-02-09 20:20:12 +0000

    fix bug in new_and_agree

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -9,7 +9,7 @@ describe do
   feature "title" do
     scenario "should have h1" do
       visit "/"
-      expect(page).to have_selector('h1', text: 'Liquid Democracy')
+      expect(page).to have_selector('h1', text: 'Who agree?')
     end
   end
 

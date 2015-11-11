5d7ca26ee4750b7fe27c4548b1720ec0299bc8a0
Hector Perez <hecpeare@gmail.com>
2015-11-11 19:31:09 +0000

    comment sign in with Twitter from header

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -12,6 +12,7 @@ feature 'next home' do
     fill_in "email", with: "hi@hectorperezarenas.com"
     click_button "See results"
     expect(page).to have_content("Should the UK remain a member of the EU?")
+    expect(page).not_to have_content("Sign in with Twitter")
     expect(BetaEmail.last.email).to eq "hi@hectorperezarenas.com"
     expect(BetaEmail.last.comment).to eq "agree"
     expect(ActionMailer::Base.deliveries.size).to eq 1

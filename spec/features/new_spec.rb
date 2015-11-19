7b0ab90d4233299bab2b57139a0c75a93f157081
Hector Perez <hecpeare@gmail.com>
2015-11-19 19:43:30 +0000

    gix spec

--- spec/features/new_spec.rb
+++ spec/features/new_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 feature "new" do
   scenario "loads" do
-    visit "/new"
-    expect(page).to have_content("What's new")
+    visit new_path
+    expect(page).to have_content("Discover new opinions")
   end
 end

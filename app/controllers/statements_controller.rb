9e8aa08041a891bd3a161ccdb71fa1fb9ff4a2b0
Hector Perez <hecpeare@gmail.com>
2015-11-25 20:56:00 +0000

    fix redirection after create_and_agree from individual page

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -38,8 +38,7 @@ class StatementsController < ApplicationController
         individual_id: params[:individual_id] || current_user.id,
         url: params[:url],
         extent: 100)
-      back = params[:back_url] || "/new"
-      redirect_to current_user.email.present? ? back : "/join?back=#{back}"
+      redirect_to params[:back_url] || new_path
     else
       flash[:error] = @statement.errors.full_messages.first
       redirect_to new_path

6f640b3814dcc71e1700c9c04813359d060f5d06
Hector Perez <hecpeare@gmail.com>
2015-11-30 18:45:48 +0000

    rm spec

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -16,7 +16,6 @@ feature 'next home' do
     scenario 'should redirect to brexit' do
       click_button "See results"
       expect(page).to have_content("Should the UK remain a member of the EU?")
-      expect(page).not_to have_content("Sign in with Twitter")
     end
 
     scenario "should save the email" do

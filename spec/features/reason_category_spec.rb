db776bc7998d80d11c9a3c3889a3a455b572266e
Hector Perez <hecpeare@gmail.com>
2016-01-30 19:41:49 +0000

    spec for reason categories

---
+++ spec/features/reason_category_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+feature "reason categories", js: true do
+  attr_reader :statement
+
+  before do
+    seed_data
+  end
+
+  context "non logged user" do
+    before do
+      login
+      visit statement_path(statement)
+    end
+
+    scenario "should set a category" do
+      click_link "Agree"
+      click_link "vote-twitter-login"
+      expect(Agreement.last.present?).to eq true
+      select "Economy", from: "reason_category_from_agreement_#{Agreement.last.id}"
+      expect(Agreement.last.reason_category.name).to eq "Economy"
+    end
+  end
+
+  def seed_data
+    @statement = create(:statement)
+    ReasonCategory.create(name: "Economy")
+    ReasonCategory.create(name: "Science")
+  end
+
+  def login
+    visit "/auth/twitter"
+  end
+end

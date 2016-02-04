e7fc3dc93b11a762591604ef3d0e49297eebf7b9
Hector Perez <hecpeare@gmail.com>
2016-02-04 18:26:52 +0000

    wait_for_ajax in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require_relative 'support/wait_for_ajax'
 require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,

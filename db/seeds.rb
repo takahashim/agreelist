aefb84c382e3ab426fff118b374b9a5d62fdc4a3
Hector Perez <hecpeare@gmail.com>
2016-10-25 21:16:44 +0100

    seeds & mv database to example

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  s = Statement.create(content: Faker::Lorem.sentence)
+  i = Individual.create(name: Faker::Name.name, bio: Faker::Lorem.sentence)
+  Agreement.create(statement: s, individual: i, extent: 100, reason: Faker::Lorem.sentence, url: Faker::Internet.url)
+end

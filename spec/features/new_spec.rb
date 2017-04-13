600825df39fa76e4ad85b2147122b7ef834a365b
Hector Perez <hecpeare@gmail.com>
2017-04-13 20:44:36 +0100

    simplify home page

--- spec/features/new_spec.rb
+++ spec/features/new_spec.rb
@@ -5,7 +5,7 @@ feature "new" do
     scenario "loads" do
       create(:statement)
       visit "/auth/twitter"
-      expect(page).to have_content("Discover new opinions")
+      expect(page).to have_content("Create a topic or statement")
     end
   end
 
@@ -13,7 +13,7 @@ feature "new" do
     scenario "loads" do
       create(:statement)
       visit "/"
-      expect(page).to have_content("Discover new opinions")
+      expect(page).to have_content("Create a topic or statement")
     end
   end
 end

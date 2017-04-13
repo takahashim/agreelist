08e0374d3e721450b323fa8a213ce571e251d9ec
Hector Perez <hecpeare@gmail.com>
2017-04-13 20:08:00 +0100

    rm others from board

--- app/models/against_others_category.rb
+++
@@ -1,5 +0,0 @@
-class AgainstOthersCategory < OthersCategory
-  def agreements
-    statement_agreements.where(extent: 0)
-  end
-end
--- app/models/in_favor_others_category.rb
+++
@@ -1,5 +0,0 @@
-class InFavorOthersCategory < OthersCategory
-  def agreements
-    statement_agreements.where(extent: 100)
-  end
-end

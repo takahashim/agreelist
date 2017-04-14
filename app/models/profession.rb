2c9a2fd7d61a9fc5957a562d6e2b0619a3aa550a
Hector Perez <hecpeare@gmail.com>
2017-04-14 15:30:05 +0100

    comment in deprecated professions

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,4 +1,5 @@
-# This was used for Brexit
+# This was used for Brexit board - not deleted in case we want to use the data
+# Currently we use occupations provided by Wikidata
 class Profession < ActiveRecord::Base
   has_many :individuals, dependent: :nullify
 end

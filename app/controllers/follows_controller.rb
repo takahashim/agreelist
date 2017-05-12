213859d3cd2854dd775fb8477b80d57b85f77337
Hector Perez <hecpeare@gmail.com>
2017-05-12 19:29:05 +0100

    fallback_location for redirect_back on FollowsController

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,15 +1,16 @@
 class FollowsController < ApplicationController
   before_action :login_required
   before_action :find_statement
+  attr_reader :fallback_location
 
   def create
     current_user.follow(@object)
-    redirect_back
+    redirect_back(fallback_location: fallback_location)
   end
 
   def destroy
     current_user.stop_following(@object)
-    redirect_back
+    redirect_back(fallback_location: fallback_location)
   end
 
   private
@@ -20,5 +21,6 @@ class FollowsController < ApplicationController
     elsif params[:individual_id]
       @object = Individual.find(params[:individual_id])
     end
+    @fallback_location = statement_path(@object)
   end
 end

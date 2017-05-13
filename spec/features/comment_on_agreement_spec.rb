e2021533ae75d5520c1e5da9cf7dadc6c0f55713
Hector Perez <hecpeare@gmail.com>
2017-05-13 12:17:08 +0100

    rm set_rack_session

--- spec/features/comment_on_agreement_spec.rb
+++ spec/features/comment_on_agreement_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'comment', js: true do
   before do
   	seed_data
-  	authenticate_as(@individual2)
+    visit "/auth/twitter"
   	visit statement_path(@statement)
     click_link "comment"
   end
@@ -12,7 +12,7 @@ feature 'comment', js: true do
   	fill_in :agreement_comment_content, with: "bla bla"
     click_button "Send"
     expect(page).to have_content("bla bla")
-    expect(page).to have_content(@individual2.name)
+    expect(page).to have_content('Hector Perez')
   end
 
 
@@ -23,9 +23,5 @@ feature 'comment', js: true do
     @individual = create(:individual)
     @individual2 = create(:individual)
     create(:agreement, individual: @individual, statement: @statement, extent: 100)
-  end
-
-  def authenticate_as(user)
-    page.set_rack_session(user_id: user.id)
   end
 end

a6003d14399f8dbc10b6c2191d80b156aa689625
Hector Perez <hecpeare@gmail.com>
2017-05-28 11:51:05 +0100

    non-found user page

--- spec/features/individual_spec.rb
+++ spec/features/individual_spec.rb
@@ -28,4 +28,11 @@ feature 'create statement from individual profile' do
     visit "/#{@hector.to_param}"
     expect(page).to have_content("Karma: #{@hector.karma}")
   end
+
+  context "non-existent individual" do
+    scenario "should return an error page" do
+      visit "/non-existent-page"
+      expect(page).to have_content("AgreeList does not have a page for non-existent-page")
+    end
+  end
 end

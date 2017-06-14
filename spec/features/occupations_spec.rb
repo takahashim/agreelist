ce1708aac88300972be373297d8de3bef9bab422
Hector Perez <hecpeare@gmail.com>
2017-06-14 20:32:21 +0100

    opinions on schools and occupations pages instead of people

--- spec/features/occupations_spec.rb
+++ spec/features/occupations_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 feature 'schools#show' do
   before do
-    create(:individual, name: "Elon Musk", twitter: "elonmusk", occupation_list: "A, B, C")
+    elon = create(:individual, name: "Elon Musk", twitter: "elonmusk", occupation_list: "A, B, C")
+    create(:agreement, individual: elon, reason: "A carbon tax is the way to go")
   end
 
-  scenario "user profile should link to occupations" do
+  scenario "user profile should link to user profile" do
     visit "/elonmusk"
     click_link "A"
     expect(page).to have_link "Elon Musk"
+  end
+
+  scenario "school should have reason" do
+    visit "/elonmusk"
+    click_link "B"
+    expect(page).to have_content "A carbon tax is the way to go"
   end
 end

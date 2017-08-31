4a92910aee4f195cde2a2733979ff4a65218c31e
Hector Perez <hecpeare@gmail.com>
2017-08-31 18:53:31 +0200

    improve admin_required for reason controller

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -1,15 +1,22 @@
 class ReasonsController < ApplicationController
+  before_action :load_agreement
+  before_action :admin_required, unless: :current_user_or_admin?
+
   def edit
-    @agreement = Agreement.find_by_hashed_id(params[:id])
   end
 
   def update
-    # do we use this or agreements#update?
-    if @agreement.individual == current_user || admin?
-      a = Agreement.update_attributes(params.require(@agreement).permit(:reason, :url))
-      redirect_to(get_and_delete_back_url || root_path, notice: "Vote/opinion added")
-    else
-      redirect_to(get_and_delete_back_url || root_path, error: "Permission denied")
-    end
+    a = Agreement.update_attributes(params.require(@agreement).permit(:reason, :url))
+    redirect_to(get_and_delete_back_url || root_path, notice: "Vote/opinion added")
+  end
+
+  private
+
+  def load_agreement
+    @agreement = Agreement.find_by_hashed_id(params[:id])
+  end
+
+  def current_user_or_admin?
+    @agreement.individual == current_user || admin?
   end
 end

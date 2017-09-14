f8bd21a99db5a09b8077be39ba26e4bea56dff17
Hector Perez <hecpeare@gmail.com>
2017-09-14 12:13:57 +0200

    display number of opinions instead of votes on search page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,7 @@ class SearchesController < ApplicationController
   end
 
   def create
-    @statements = Statement.where("content ilike ?", "%#{params[:search]}%").select("count(agreements.id) as opinions_count, statements.id, statements.url, statements.content").joins(:agreements).group("statements.id").order("opinions_count DESC").to_a
-        # @statements = Statement.where("content ilike ?", "%#{params[:search]}%").select("count(agreements.id) as opinions_count, statements.id, statements.url, statements.content").joins(:agreements).where("agreements.reason is not null and agreements.reason != ''").group("statements.id").order("opinions_count DESC").to_a
-
+    @statements = Statement.where("content ilike ?", "%#{params[:search]}%").select("statements.id, statements.opinions_count, statements.url, statements.content").group("statements.id").order("opinions_count DESC").to_a
     @individuals = Individual.where("name ilike ?", "%#{params[:search]}%")
     @agreements = Agreement.where("reason ilike ?", "%#{params[:search]}%").order(upvotes_count: :desc).order(upvotes_count: :desc)
   end

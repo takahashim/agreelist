3b516f0d6b36e3eee401efe00ea07870eb3f1eae
Hector Perez <hecpeare@gmail.com>
2017-09-25 18:22:08 +0100

    display user search on url parameter

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,19 @@
 class SearchesController < ApplicationController
   def new
+    if params[:q]
+      @statements = Statement.where("content ilike ?", "%#{params[:q]}%").select("statements.id, statements.opinions_count, statements.url, statements.content").group("statements.id").order("opinions_count DESC").to_a
+      @individuals = Individual.where("name ilike ?", "%#{params[:q]}%")
+      @agreements = Agreement.where("reason ilike ?", "%#{params[:q]}%").order(upvotes_count: :desc).order(upvotes_count: :desc)
+      @page_type = 'search results page'
+    end
   end
 
   def create
-    @statements = Statement.where("content ilike ?", "%#{params[:search]}%").select("statements.id, statements.opinions_count, statements.url, statements.content").group("statements.id").order("opinions_count DESC").to_a
-    @individuals = Individual.where("name ilike ?", "%#{params[:search]}%")
-    @agreements = Agreement.where("reason ilike ?", "%#{params[:search]}%").order(upvotes_count: :desc).order(upvotes_count: :desc)
+    if params[:search].present?
+      redirect_to search_path(q: params[:search])
+    else
+      flash.now[:notice] = "Search field can't be empty"
+      render action: :new
+    end
   end
 end
